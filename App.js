c716bbfc4e757301695892dbd98bc9950f4b5f40
alejandro de jesus <alejandro.dejesus@fluxit.com.ar>
2020-05-24 23:05:19 -0300

    initialazing

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import Header from './components/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header  title="Guess a number"/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
---
+++ components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const Header = props => {
+    return (
+        <View style = {styles.header}>
+            <Text>{props.title}</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        height: 90,
+        paddingTop: 36,
+        backgroundColor: '#f1287b'
+    },
+    headerTilte: {
+        color: 'black',
+        fontSize: 18
+    }
+});
+
+export default Header;
